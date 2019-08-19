d35dbe2040c9436f26db1f76713e78947cde57bc
shahzaib-ikram <z5144829@ad.unsw.edu.au>
2019-08-19 17:16:02 +0500

    🐱‍👤 stable version login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { IconsProviderModule } from './icons-provider.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    IconsProviderModule,
+    NgZorroAntdModule,
+    ReactiveFormsModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }, FormBuilder],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this._fb.group({
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      remember: [true, Validators.required]
+    });
+  }
+
+  login() {
+    if (!this.loginForm.valid) return;
+    console.log(this.loginForm.value);
+  }
+
+  private get email() {
+    return this.loginForm.get('email');
+  }
+  private get password() {
+    return this.loginForm.get('password');
+  }
+  private get remember() {
+    return this.loginForm.get('remember');
+  }
+}
